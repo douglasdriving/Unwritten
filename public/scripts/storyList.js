5304ce4ea08f5989c78b9d2e059b264cd52fa51f
douglasdriving <douglasdriving@gmail.com>
2022-06-09 15:30:54 +0700

    Update storyList.js

--- public/scripts/storyList.js
+++ public/scripts/storyList.js
@@ -6,28 +6,37 @@ LoadStoryMeny();
 
 async function LoadStoryMeny() {
 
-    const loadText = document.createElement('p');
-    storyDiv.append(loadText);
-    loadText.textContent = 'Loading unwritten stories...';
-
-    const stories = await getStories();
-
-    await Promise.all(stories.map(async (story) => {
-        const count = await getScenarioCount(story.id);
-        story.scenarioCount = count;
-    }))
-
-    stories.sort(function (a, b) {
-        if (a.scenarioCount < b.scenarioCount) return 1;
-        if (a.scenarioCount > b.scenarioCount) return -1;
-        return 0;
-    });
-
+    const loadText = DisplayLoadText();
+    const stories = await GetSortedStories();
     stories.forEach(story => {
         AddStoryToMeny(story.data().title, story.data().description, story.id, story.scenarioCount);
     });
-
     loadText.remove();
+
+    function DisplayLoadText() {
+        const loadText = document.createElement('p');
+        storyDiv.append(loadText);
+        loadText.textContent = 'Loading unwritten stories...';
+        return loadText;
+    }
+
+    async function GetSortedStories() {
+        const stories = await getStories();
+
+        await Promise.all(stories.map(async (story) => {
+            const count = await getScenarioCount(story.id);
+            story.scenarioCount = count;
+        }));
+
+        stories.sort(function (a, b) {
+            if (a.scenarioCount < b.scenarioCount)
+                return 1;
+            if (a.scenarioCount > b.scenarioCount)
+                return -1;
+            return 0;
+        });
+        return stories;
+    }
 }
 
 async function AddStoryToMeny(title, description, id, scenarioCount) {
@@ -35,28 +44,19 @@ async function AddStoryToMeny(title, description, id, scenarioCount) {
     const menuItem = document.createElement('div');
     menuItem.className = 'story';
     storyDiv.append(menuItem);
-
-    const heading = document.createElement('h3');
-    heading.textContent = title;
-    heading.className = 'storyItemText';
-    menuItem.append(heading);
-
-    const descText = document.createElement('p');
-    descText.className = 'storyItemText';
-    descText.textContent = description;
-    menuItem.append(descText);
-
-    const scenarioCounter = document.createElement('p');
-    scenarioCounter.className = 'storyItemText';
-    scenarioCounter.textContent = ('(' + scenarioCount + ' scenarios)');
-    menuItem.append(scenarioCounter);
-
-    const startStoryButton = document.createElement('button');
-    startStoryButton.textContent = 'Play this story';
-    startStoryButton.className = 'startStoryButton';
-    menuItem.append(startStoryButton);
-
+    CreateRow('h3', title, 'storyItemText');
+    CreateRow('p', description, 'storyItemText');
+    CreateRow('p', `(${scenarioCount} scenarios)`, 'storyItemText');
+    const startStoryButton = CreateRow('button', 'Play this story', 'startStoryButton');
     startStoryButton.onclick = () => {
         window.location.href = `/pages/play.html?v=0.11&storyCollectionID=${id}`;
+    }
+
+    function CreateRow(type, text, className) {
+        const element = document.createElement(type);
+        element.textContent = text;
+        element.className = className;
+        menuItem.append(element);
+        return element;
     }
 }
